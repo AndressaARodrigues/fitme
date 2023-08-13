b9603eca5187e0f42c3cf84934fdf256e7b5295e
AndressaARodrigues <andressa.rodruigues33@gmail.com>
2023-08-13 01:11:27 -0300

    Configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import ErrorPage from './pages/Error';
+import HomePage from './pages/Home';
+import RootLayout from './pages/Root';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
   <>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>}/>
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </Router>
   </>
   );
 }
